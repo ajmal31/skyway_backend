8306e9c2995d4b4e3154cf55fdbdb9d0ab026b79
ajmal31 <ajmalmuhammed846@gmail.com>
2023-12-08 11:20:52 +0530

    chat service handled venture data (chaters) v40

--- chat-srv/src/Message-Broker/consumer/consumer.ts
+++ chat-srv/src/Message-Broker/consumer/consumer.ts
@@ -1,16 +1,31 @@
 import createChannel from "../config";
+import { findChater } from "../../Helper/chatHelper";
+import { insertChatersDetails } from "../../Helper/chatHelper";
 
 const consumer = async () => {
 
     const channel = await createChannel()
-    channel.consume("CHAT-SRV", (data) => {
+    channel.consume("CHAT-SRV", async (data) => {
         if (data) {
-            console.log('chat service data consumed', JSON.parse(data?.content?.toString()));
+            const message = JSON.parse(data?.content?.toString());
+
+            if (message?.ventureName) {
+
+                const existDoc = await findChater(message._id)
+                if (existDoc) {
+                    console.log('the Document already exist in the chaters')
+                    return channel.ack(data)
+                }
+                const response = await insertChatersDetails(message)
+                return channel.ack(data)
+
+
+            }
             channel.ack(data)
 
         }
         else console.log('chat service while consuming data not found')
-        
+
 
     })
 
