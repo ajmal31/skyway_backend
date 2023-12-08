b69930adee7d186f27dda3edadce0258b029602b
ajmal31 <ajmalmuhammed846@gmail.com>
2023-12-08 12:20:01 +0530

    user service fetch details and publish to chat-srv and chat-srv handled v40

--- chat-srv/src/Message-Broker/consumer/consumer.ts
+++ chat-srv/src/Message-Broker/consumer/consumer.ts
@@ -8,10 +8,12 @@ const consumer = async () => {
     channel.consume("CHAT-SRV", async (data) => {
         if (data) {
             const message = JSON.parse(data?.content?.toString());
-
-            if (message?.ventureName) {
+            console.log('chat service consuming',message)
+            //These conditon handling Venture and User data for updating chaters
+            if (message?.ventureName||message?.username) {
 
                 const existDoc = await findChater(message._id)
+                //check whether Document already Exist or not
                 if (existDoc) {
                     console.log('the Document already exist in the chaters')
                     return channel.ack(data)
