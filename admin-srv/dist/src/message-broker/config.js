9124ec64d7195f4cf0e944c3cbb1e2dc70e7a376
ajmal31 <ajmalmuhammed846@gmail.com>
2023-12-16 20:47:15 +0530

    admin service dockerized

--- admin-srv/dist/src/message-broker/config.js
+++ admin-srv/dist/src/message-broker/config.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const amqplib_1 = __importDefault(require("amqplib"));
 const createChannel = async () => {
     const uri = 'amqp://localhost';
+    // const uri='amqp://host.docker.internal:5672'
     const connection = await amqplib_1.default.connect(uri);
     const channel = await connection.createChannel();
     const queueName = 'ADMIN-SRV';
--- admin-srv/src/message-broker/config.ts
+++ admin-srv/src/message-broker/config.ts
@@ -2,6 +2,7 @@ import amqp from 'amqplib'
 const createChannel=async()=>{
 
     const uri='amqp://localhost'
+    // const uri='amqp://host.docker.internal:5672'
     const connection=await amqp.connect(uri)
     const channel=await connection.createChannel()
     const queueName='ADMIN-SRV'
